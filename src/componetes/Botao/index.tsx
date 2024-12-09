e829785c3f9754c1bdbd604cae4cfd806676d0b3
unknown <diegoleweb@gmail.com>
2024-12-09 20:37:04 -0300

    Adicionando o projeto final

--- src/componetes/Botao/index.tsx
+++ src/componetes/Botao/index.tsx
@@ -1,9 +1,8 @@
 import { ReactElement } from 'react'
 import './Botao.css'
-import React from 'react' // Fazendo o import do react e abilitando ele no tsconfig 
 
 interface BotaoProps {
-    children : ReactElement //Criando uma interface para a props e importando o ReactElement
+    children : ReactElement  | string //Criando uma interface para a props e importando o ReactElement
 }
 
 const Botao = (props: BotaoProps) =>{ //colocou a tipagem da props com 'BotaoProps'
